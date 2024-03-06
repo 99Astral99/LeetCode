01f428da1279f3a35ffb2784b2f41bbed77692e9
Viktor <49991303+99Astral99@users.noreply.github.com>
2024-03-06 13:19:58 +0300

    Time: 224 ms (10.44%) | Memory: 47.5 MB (17.23%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cs
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.cs
@@ -1,7 +1,7 @@
 public class Solution {
     public int[] SmallerNumbersThanCurrent(int[] nums) {
         var smaller = new int[nums.Length];
-
+        
         for (int i = 0; i < nums.Length; i++)
         {
             smaller[i] = nums.Where(x => x < nums[i]).Count();
